16481e7b6ab2b1a84c19b20e480efc149cc6efb9
Amna Sarfraz <107910896+Amna-S@users.noreply.github.com>
2024-09-22 22:10:05 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main ()
+{
+    std::cout<<"Hello, Welcome to my coding lesson."<<"\n";
+    std::cout<<"How are you?\n";
+    std::cout<<"How was your today's lesson?"<<std::endl;
+    std::cout<<"I had a good lesson!";
+    return 0;
+}
